b1c7b428a21ad86431167e897e1a598c480ef0b3
matthi1304 <>
2025-11-26 19:25:20 +0100

    more config options in .env plus minor changes

--- genetics.py
+++ genetics.py
@@ -9,20 +9,50 @@ from fitness import FitnessFunction
 from dotenv import load_dotenv
 from random import random, randint
 
-RADIUS = 15000
+# parameters for the genetic algorithm (genetics.py) 
+#
+# number of genes per individual (number of placed digits in the configuration)
+SIZE_OF_GENOM=220
+
+# number of individuals per generation competing
+SIZE_OF_GENERATION=400
+
+# rate of best individuals surviving and being parents for the next generation
+SURVIVOR_RATE=0.8
+
+# rate of individuals being mutated per generation (only applies to new offsprings)
+MUTATION_RATE=0.7
+
+# maximum number of parents for crossover. must be >= 2
+MAX_PARENTS = 3
+
+# maximumnumber of generations
+TOURNAMENT_SIZE=5000
+
+# path for fitness images
+FITNESS_IMAGE_PATH="img/"
+
+# images to compare against for fitness evaluation
+FITNESS_IMAGES="h_1.png,h_2.png,h_3.png,h_4.png,h_5.png,h_6.png,h_7.png,h_8.png,h_9.png,h_10.png,h_11.png,h_12.png"
+
+# camera distance from the center of the sphere
+CAMERA_DISTANCE = -60
+
+# level of quantization for position (x,y,z)
 QUANTIZATION = 100
-Q = RADIUS // QUANTIZATION # the following condition should hold: Q * QUANTIZATION == RADIUS 
 
+# radius of the sphere for placement of digits
+RADIUS = 15000
+
+# scale limits for the digits
+# in real life this should be at least 40 mm
+MIN_SCALE =  600 
 MAX_SCALE = 6000
-MIN_SCALE =  100 # in real life this should be at least 40 mm
 
-NUM_PARENTS = 3 # must be at least 2
+# prefer less visual noise around image
+FITNESS_FUNCTION_FACTOR = 1.1
 
-DEFAULT_NUM_CHROMOSOMS = 100
-DEFAULT_SIZE_OF_GENERATION = 50
-DEFAULT_SURVIVOR_RATE = 0.3
-DEFAULT_MUTATION_RATE = 0.1
-DEFAULT_TOURNAMENT_SIZE = 100
+Q = RADIUS // QUANTIZATION # the following condition should hold: Q * QUANTIZATION == RADIUS 
 
 class Individual:
 
@@ -53,6 +83,7 @@ class Individual:
     @staticmethod
     def random_individual(size_of_genom):
         genom = [Individual.create_random_gene() for _ in range(size_of_genom)]
+        genom.sort(key=lambda gene: (gene[1], gene[2]))  # sort by x, y
         return Individual(genom)
     
 
@@ -129,42 +160,33 @@ class Individual:
 
 class Genetics:
 
-    def __init__(self, 
-                 size_of_genom=DEFAULT_NUM_CHROMOSOMS, 
-                 size_of_generation=DEFAULT_SIZE_OF_GENERATION,
-                 survivor_rate=DEFAULT_SURVIVOR_RATE, 
-                 mutation_rate=DEFAULT_MUTATION_RATE,
-                 tournament_size=DEFAULT_TOURNAMENT_SIZE,
-                 target_images=None):
-        self.size_of_genom = size_of_genom
-        self.size_of_generation = size_of_generation
-        self.survivor_rate = survivor_rate
-        self.mutation_rate = mutation_rate
-        self.tournament_size = tournament_size
+    def __init__(self, target_images):
+        self.mutation_rate = MUTATION_RATE
         self.target_images = target_images
         self.winner = None
 
 
     def run(self, app):
-        fitness_function = FitnessFunction(app, self.target_images).fitness_function
-        population = self.create_random_population(self.size_of_generation)
+        app.set_camera_distance(CAMERA_DISTANCE)
+        fitness_function = FitnessFunction(app, self.target_images, FITNESS_FUNCTION_FACTOR).fitness_function
+        population = self.create_random_population(SIZE_OF_GENERATION)
         self.winner = population[0].genom
         print("=============================================================")
         print("starting genetic algorithm...")
         print("=============================================================")
         worst_survivor_fitness = -1000.0
         stagnation_count = 0
-        generation = -1
+        self.generation = -1
         while True:
-            generation += 1
+            self.generation += 1
             start_time = time()
             population.sort(key=lambda x: x.getFitness(fitness_function=fitness_function), reverse=True)
             self.winner = population[0].genom # store the best individuals genom
-            num_survivors = int(self.size_of_generation * self.survivor_rate)
+            num_survivors = int(SIZE_OF_GENERATION * SURVIVOR_RATE)
             survivors = population[:num_survivors]
-            self.log_stats(population, generation, start_time, survivors)
+            self.log_stats(population, start_time, survivors)
             population = []
-            if generation >= self.tournament_size:
+            if self.generation >= TOURNAMENT_SIZE:
                 # final generation, do not breed further
                 return
             if worst_survivor_fitness == survivors[-1].getFitness():
@@ -174,21 +196,21 @@ class Genetics:
                     # alternative breed starts from scratch
                     print(f"Warn: Stagnation ({stagnation_count}) detected, develop new breed to mix in")
                     survivors += self.alternative_breed(
-                        fitness_function, survivors[0].getFitness(), generation)
+                        fitness_function, survivors[0].getFitness())
                 elif stagnation_count <= 3:
                     # seed alternative breed
                     print(f"Warn: Stagnation ({stagnation_count}) detected, develop new breed to mix in")
                     l = len(survivors) - 1
                     survivors += self.alternative_breed(
-                        fitness_function, survivors[0].getFitness(), generation, population=sample(survivors[1:], l // 2))
+                        fitness_function, survivors[0].getFitness(), population=sample(survivors[1:], l // 2))
                 elif stagnation_count >= 10:
                     print(f"Warn: Termininating after #{stagnation_count} stagnations")
                     return
             else:
                 worst_survivor_fitness = survivors[-1].getFitness()
             # Breeding
-            while len(population) + len(survivors) < self.size_of_generation:
-                child = Individual.breed(sample(survivors, NUM_PARENTS))
+            while len(population) + len(survivors) < SIZE_OF_GENERATION:
+                child = Individual.breed(sample(survivors, MAX_PARENTS))
                 population.append(child)
             # Mutation
             for _ in range(int(len(population) * self.mutation_rate)):
@@ -198,28 +220,28 @@ class Genetics:
             population += survivors
 
 
-    def alternative_breed(self, fitness_function, target_fitness, generation, population=[]):
+    def alternative_breed(self, fitness_function, target_fitness, population=[]):
         # additional breeding to reach target fitness
-        population += self.create_random_population(self.size_of_generation - len(population))
+        population += self.create_random_population(SIZE_OF_GENERATION - len(population))
         last_worst_fitness = -1000.0
         while True:
-            generation += 1
+            self.generation += 1
             start_time = time()
             population.sort(key=lambda x: x.getFitness(fitness_function=fitness_function), reverse=True)
-            num_survivors = int(self.size_of_generation * self.survivor_rate)
+            num_survivors = int(SIZE_OF_GENERATION * SURVIVOR_RATE)
             survivors = population[:num_survivors]
-            self.log_stats(population, generation, start_time, survivors)
+            self.log_stats(population, start_time, survivors)
             winner = survivors[0]
             worst_survivor = survivors[-1]
             population = []  
-            if (generation >= self.tournament_size) or (winner.getFitness() >= target_fitness) or (last_worst_fitness == worst_survivor.getFitness()):
+            if (self.generation >= TOURNAMENT_SIZE) or (winner.getFitness() >= target_fitness) or (last_worst_fitness == worst_survivor.getFitness()):
                 # do not breed further
                 print("Info: Ending alternative breeding phase.")
                 return survivors
             last_worst_fitness = survivors[-1].getFitness()
             # Breeding
-            while len(population) + len(survivors) < self.size_of_generation:
-                child = Individual.breed(sample(survivors, NUM_PARENTS))
+            while len(population) + len(survivors) < SIZE_OF_GENERATION:
+                child = Individual.breed(sample(survivors, MAX_PARENTS))
                 population.append(child)
             # Mutation
             for _ in range(int(len(population) * self.mutation_rate)):
@@ -229,13 +251,13 @@ class Genetics:
             population += survivors
 
 
-    def log_stats(self, population, generation, start_time, survivors):
-        if generation % 25 == 0:
+    def log_stats(self, population, start_time, survivors):
+        if self.generation % 25 == 0:
             print( "=============================================================")
             print("           best     worst   average   average")
             print("    #  survivor  survivor survivors       all  duration")
             #      ----+----|----+----|----+----|----+----|----+----|----+----|
-        print(f"{generation:5d}"+
+        print(f"{self.generation:5d}"+
                   f"{survivors[0].getFitness():10.5f}"+
                   f"{survivors[-1].getFitness():10.5f}"+
                   f"{sum(ind.getFitness() for ind in survivors) / len(survivors):10.5f}"+
@@ -246,22 +268,43 @@ class Genetics:
     def create_random_population(self, count):
         population = []
         for _ in range(count):
-            population.append(Individual.random_individual(self.size_of_genom))
+            population.append(Individual.random_individual(SIZE_OF_GENOM))
         return population
 
 
 def _get_from_env(var_name, default, cast_type):
     value = os.getenv(var_name)
     if value is None:
-        print(f"Info: Using {var_name}={default} (default)")
-    else:
-        try:
-            value = cast_type(value)
-            print(f"Info: Using {var_name}={value} (.env / environment)")
-            return value
-        except ValueError:
-            print(f"Warning: Could not cast environment variable {var_name}='{value}' to {cast_type}. Using default {default}.")
+        value = default
+    try:
+        value = cast_type(value)
+        print(f"Info: Using {var_name}={value}{'' if value != default else ' (default)'}")
+        return value
+    except ValueError:
+        print(f"Warning: Could not cast environment variable {var_name}='{value}' to {cast_type}. Using default {default}.")
     return default
+
+
+def load_settings():
+    load_dotenv()
+    global SIZE_OF_GENOM, SIZE_OF_GENERATION, SURVIVOR_RATE, MUTATION_RATE
+    global TOURNAMENT_SIZE, FITNESS_IMAGE_PATH, FITNESS_IMAGES
+    global CAMERA_DISTANCE, QUANTIZATION, RADIUS, MIN_SCALE, MAX_SCALE
+    global FITNESS_FUNCTION_FACTOR, Q
+    SIZE_OF_GENOM = _get_from_env("SIZE_OF_GENOM", SIZE_OF_GENOM, int)
+    SIZE_OF_GENERATION = _get_from_env("SIZE_OF_GENERATION", SIZE_OF_GENERATION, int)
+    SURVIVOR_RATE = _get_from_env("SURVIVOR_RATE", SURVIVOR_RATE, float)
+    MUTATION_RATE = _get_from_env("MUTATION_RATE", MUTATION_RATE, float)
+    TOURNAMENT_SIZE = _get_from_env("TOURNAMENT_SIZE", TOURNAMENT_SIZE, int)
+    FITNESS_IMAGE_PATH = _get_from_env("FITNESS_IMAGE_PATH", FITNESS_IMAGE_PATH, str)
+    FITNESS_IMAGES = _get_from_env("FITNESS_IMAGES", FITNESS_IMAGES, str)
+    CAMERA_DISTANCE = _get_from_env("CAMERA_DISTANCE", CAMERA_DISTANCE, int)
+    QUANTIZATION = _get_from_env("QUANTIZATION", QUANTIZATION, int)
+    RADIUS = _get_from_env("RADIUS", RADIUS, int)
+    MIN_SCALE = _get_from_env("MIN_SCALE", MIN_SCALE, int)
+    MAX_SCALE = _get_from_env("MAX_SCALE", MAX_SCALE, int)
+    FITNESS_FUNCTION_FACTOR = _get_from_env("FITNESS_FUNCTION_FACTOR", FITNESS_FUNCTION_FACTOR, float)
+    Q = RADIUS // QUANTIZATION
 
 
 if __name__ == "__main__":
@@ -269,27 +312,13 @@ if __name__ == "__main__":
     print("=============================================================")
     print("Info: Starting genetic algorithm... (cancel with Ctrl-C)")
     print("=============================================================")
-    load_dotenv()
-    SIZE_OF_GENOM = _get_from_env("SIZE_OF_GENOM", 100, int)
-    SIZE_OF_GENERATION = _get_from_env("SIZE_OF_GENERATION", 50, int)
-    SURVIVOR_RATE = _get_from_env("SURVIVOR_RATE", 0.3, float)
-    MUTATION_RATE = _get_from_env("MUTATION_RATE", 0.1, float)
-    TOURNAMENT_SIZE = _get_from_env("TOURNAMENT_SIZE", 100, int)
-    TARGET_IMAGE_PATH = _get_from_env("FITNESS_IMAGE_PATH", "", str)
-    TARGET_IMAGES = _get_from_env("FITNESS_IMAGES", "", str)
+    load_settings()
     print("=============================================================")
-    assert len(TARGET_IMAGES) > 0, "Error: No FITNESS_IMAGES specified in .env file."
-    target_images = [TARGET_IMAGE_PATH + img for img in TARGET_IMAGES.split(",")]
+    assert len(FITNESS_IMAGES) > 0, "Error: No FITNESS_IMAGES specified in .env file."
+    target_images = [FITNESS_IMAGE_PATH + img for img in FITNESS_IMAGES.split(",")]
     for img in target_images:
         assert os.path.isfile(img), f"Error: Target image file '{img}' does not exist."
-    genetics = Genetics(
-        size_of_genom=SIZE_OF_GENOM,
-        size_of_generation=SIZE_OF_GENERATION,
-        survivor_rate=SURVIVOR_RATE,
-        mutation_rate=MUTATION_RATE,
-        tournament_size=TOURNAMENT_SIZE,
-        target_images=target_images
-    )
+    genetics = Genetics(target_images)
     try:
         visualization.headless_app(callback=genetics.run, prc_file="headless_128x128.prc")
     except KeyboardInterrupt:
@@ -298,12 +327,6 @@ if __name__ == "__main__":
     filename = "winner.%d.json" % int(time()) if len(sys.argv) < 2 else sys.argv[1]
     with open(filename, "w") as f:
         f.write(json_str)
-    print("=============================================================")
-    print(f"Info: SIZE_OF_GENOM        {SIZE_OF_GENOM}")
-    print(f"Info: SIZE_OF_GENERATION   {SIZE_OF_GENERATION}")
-    print(f"Info: SURVIVOR_RATE        {SURVIVOR_RATE}")
-    print(f"Info: MUTATION_RATE        {MUTATION_RATE}")
-    print(f"Info: TOURNAMENT_SIZE      {TOURNAMENT_SIZE}")
     print("=============================================================")
     print(f"Info: Total time elapsed   {time() - start_time:.3f} seconds")
     print(f"Info: Winner configuration saved to {filename}")
