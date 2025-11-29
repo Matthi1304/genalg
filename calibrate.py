7a23e5549beb41ecf3a1300c38880fb919f76a56
matthi1304 <>
2025-11-29 02:17:54 +0100

    new key bindings

--- calibrate.py
+++ calibrate.py
@@ -1,18 +1,6 @@
 # calibrate the beamer image:
-# simple application showing a black image with a white circle in the center
-# the circle can be moved with the mouse and the size can be changed while holding the left mouse button
-# and dragging the mouse up and down or left and right
-# when pressing any number key 0-9 this number is placed in the center of the circle in red color
-# the number with the spot can then be moved around and scaled as well
-# when pressing s the number is drawn in green color and the position of the number is then fixed
-# the white spot can then be moved around again to place the next number
-# pressing r removes the number at the current spot position
-# pressing c lets the user change the current number and re-enables moving and scaling
-# of the number at the spot position
-# pressing space changes the background color from black to white and vice versa
-# pressing q quits the application and saves the configuration to a json file
-# this way the beamer can be calibrated to fit the hour plate
-
+# simple application to place digits at certain positions
+# load and save the configuration to a json file
 from logging import config
 import os
 import sys
@@ -36,10 +24,8 @@ class CalibrationApp(ShowBase):
     
         self.config_file = config_file
         # State variables
-        self.spot_pos = Point2(0, 0)
         self.spot_size = 0.3
-        self.dragging = False
-        self.last_mouse_pos = None
+        self.shiftDown = False
         # Storage for placed numbers
         self.placed_numbers = []  # [{digit, x, y, scale, text_node},..]
         # Current number text
@@ -63,16 +49,48 @@ class CalibrationApp(ShowBase):
 
         
         # Setup mouse and keyboard events
-        self.accept("mouse1", self.on_mouse_down)
-        self.accept("mouse1-up", self.on_mouse_up)
-        self.accept("s", self.fix_number)
-        self.accept("r", self.remove_number)
-        self.accept("c", self.change_number)
+        self.accept("mouse1", lambda: self.fix_number() if self.current_text else self.change_number())
+        self.accept("mouse3", self.remove_number)
+        self.accept("wheel_up", self.resize, [0.01])
+        self.accept("wheel_down", self.resize, [-0.01])
+
+        self.accept("space", lambda: self.fix_number() if self.current_text else self.change_number())
+
+        self.accept("shift-wheel_up", self.increase_number, [1])
+        self.accept("shift-wheel_down", self.increase_number, [-1])
+
+        self.accept("arrow_up", self.move, [0, 0.001])
+        self.accept("arrow_up-repeat", self.move, [0, 0.001])
+        self.accept("arrow_down", self.move, [0, -0.001])
+        self.accept("arrow_down-repeat", self.move, [0, -0.001])
+        self.accept("arrow_right", self.move, [0.001, 0])
+        self.accept("arrow_right-repeat", self.move, [0.001, 0])
+        self.accept("arrow_left", self.move, [-0.001, 0])
+        self.accept("arrow_left-repeat", self.move, [-0.001, 0])
+
+        self.accept("shift-arrow_up", self.resize, [0.01])
+        self.accept("shift-arrow_up-repeat", self.resize, [0.01])
+        self.accept("shift-arrow_down", self.resize, [-0.01])
+        self.accept("shift-arrow_down-repeat", self.resize, [-0.01])
+        self.accept("shift-arrow_right", self.resize, [0.01])
+        self.accept("shift-arrow_right-repeat", self.resize, [0.01])
+        self.accept("shift-arrow_left", self.resize, [-0.01])
+        self.accept("shift-arrow_left-repeat", self.resize, [-0.01])
+
+        self.accept("control-arrow_left", self.roll_number, [-0.1])
+        self.accept("control-arrow_left-repeat", self.roll_number, [-0.1])
+        self.accept("control-arrow_right", self.roll_number, [0.1])
+        self.accept("control-arrow_right-repeat", self.roll_number, [0.1])
+
+        self.accept("shift-control-arrow_left", self.rotate_number, [-0.01])
+        self.accept("shift-control-arrow_left-repeat", self.rotate_number, [-0.01])
+        self.accept("shift-control-arrow_right", self.rotate_number, [0.01])
+        self.accept("shift-control-arrow_right-repeat", self.rotate_number, [0.01])
+
         self.accept("q", self.quit_and_save)
-        self.accept("w", self.save)
+        self.accept("s", self.save)
         self.accept("f", self.toggle_fullscreen)
         self.accept("i", self.print_stats)
-        self.accept("a", self.toggle_animation)
         self.accept("h", self.toggle_help)
         self.accept("escape", sys.exit)
 
@@ -82,19 +100,20 @@ class CalibrationApp(ShowBase):
         
         print("=======================================================================")        
         self.onScreenText(f"Config: {self.config_file}")
-        self.onScreenText("Use mouse to move the spot, press and hold left mouse button to resize.")
+        self.onScreenText("Use mouse to move the spot, use mouse wheel to resize.")
+        self.onScreenText("Click to set digit, right click to remove digit.")
+        self.onScreenText("Use arrow keys to nudge the digit position.")
+        self.onScreenText("Use shift + mouse wheel to change between digits.")
+        self.onScreenText("Use shift + arrow keys to resize digit.")
+        self.onScreenText("Use ctrl + arrow left/right to rotate digit (ctrl + shift for vertical rotation).")
+        self.onScreenText("Space to fix/unfix digit.")
         self.onScreenText("h = hide/show this help")
-        self.onScreenText("0-9 = place digit")
         self.onScreenText("f = toggle fullscreen")
-        self.onScreenText("s = fix digit")
-        self.onScreenText("r = remove digit")
-        self.onScreenText("c = change digit")
         self.onScreenText("i = print statistics")
-        self.onScreenText("a = start/stop clock animation")
-        self.onScreenText("w = save configuration")
-        self.onScreenText("q = save and quit")
+        self.onScreenText("s = save configuration")
+        self.onScreenText("q = quit (and save configuration)")
         self.onScreenText("escape = quit without saving")
-        print("=======================================================================", 11)
+        print("=======================================================================")
 
         # Create on screen text for xy position
         self.xyText = self.onScreenText("0.00:0.00")
@@ -139,7 +158,6 @@ class CalibrationApp(ShowBase):
         img = PNMImage(size, size, 4)
         center = size // 2
         radius = size // 2 - 2
-        
         for y in range(size):
             for x in range(size):
                 dx = x - center
@@ -150,19 +168,16 @@ class CalibrationApp(ShowBase):
                     img.setAlpha(x, y, 1)
                 else:
                     img.setAlpha(x, y, 0)
-        
         tex = Texture()
         tex.load(img)
-        
         # Create card for the spot
         cm = CardMaker("spot")
         cm.setFrame(-1, 1, -1, 1)
-        
         self.spot = self.aspect2d.attachNewNode(cm.generate())
         self.spot.setTexture(tex)
         self.spot.setTransparency(TransparencyAttrib.MAlpha)
         self.spot.setScale(self.spot_size)
-        self.spot.setPos(self.spot_pos.x, 0, self.spot_pos.y)
+        self.spot.setPos(0, 0, 0)
 
 
     def toggle_fullscreen(self):
@@ -177,7 +192,6 @@ class CalibrationApp(ShowBase):
         """Get mouse position in 2D aspect2d coordinates"""
         if not self.mouseWatcherNode.hasMouse():
             return None
-        
         mouse_pos = self.mouseWatcherNode.getMouse()
         # Convert to aspect2d coordinates
         aspect_ratio = self.getAspectRatio()
@@ -187,16 +201,18 @@ class CalibrationApp(ShowBase):
         return Point2(x, y)
 
 
-    def on_mouse_down(self):
-        """Handle mouse button down"""
-        self.dragging = True
-        self.last_mouse_pos = self.get_mouse_pos_2d()
-
-
-    def on_mouse_up(self):
-        """Handle mouse button up"""
-        self.dragging = False
-        self.last_mouse_pos = None
+    def increase_number(self, delta):
+        """Increase or decrease the current number"""
+        if self.current_text:
+            digit = self.current_text.text
+            digit = 0 if digit == 'O' else int(digit)
+            digit = (digit + delta) % 10
+            text = 'O' if digit == 0 else str(digit)
+            self.current_text.text = text
+        elif delta > 0:
+            self.place_number(1)
+        else:
+            self.place_number(9)
 
 
     def place_number(self, digit):
@@ -236,7 +252,7 @@ class CalibrationApp(ShowBase):
 
 
     def getNumberPosition(self):
-        return (self.spot_pos.x, self.spot_pos.y - self.spot_size/2)
+        return (self.spot.getX(), self.spot.getZ() - self.spot_size/2)
 
 
     def numberAtMouse(self, tolerance=1):
@@ -244,8 +260,8 @@ class CalibrationApp(ShowBase):
         for data in self.placed_numbers:
             x = data['x']
             y = data['y']
-            dist = ((x - self.spot_pos.x)**2 + 
-                   (y - self.spot_pos.y)**2)**0.5
+            dist = ((x - self.spot.getX())**2 + 
+                   (y - self.spot.getZ())**2)**0.5
             if dist < tolerance:
                 item = data
                 tolerance = dist  # Update tolerance to closest
@@ -268,22 +284,14 @@ class CalibrationApp(ShowBase):
         """Change/edit the number at current spot position"""
         if self.current_text:
             return  # Already editing a number
-
-        # Find if there's a fixed number near the spot position
         item = self.numberAtMouse()
-        
         if item is not None:
-            # Remove from placed numbers and make it current
             item['text_node'].removeNode()
             self.placed_numbers.remove(item)
-            
             # Recreate as current (red) number
-            self.spot_pos = Point2(item['x'], item['y'])
-            self.spot_size = item['scale'] / 2
-            
-            self.spot.setPos(self.spot_pos.x, 0, self.spot_pos.y)
+            self.spot_size = item['scale'] / 2            
+            self.spot.setPos(item['x'], 0, item['y'])
             self.spot.setScale(self.spot_size)
-
             self.current_text = OnscreenText(
                 text=str(item['digit']),
                 pos=self.getNumberPosition(),
@@ -330,18 +338,16 @@ class CalibrationApp(ShowBase):
 
     def print_stats(self):
         """Print statistics of placed numbers"""
-
         print("Available display sizes:")
         di = base.pipe.getDisplayInformation()
         for index in range(di.getTotalDisplayModes()):
             print(f"{di.getDisplayModeWidth(index)}x{di.getDisplayModeHeight(index)}")  
-
         self.placed_numbers.sort(key=SORT)
         digit_counters = [0 for _ in range(10)]
         for item in self.placed_numbers:
             digit_counters[item['digit']] += 1
         # we need 5 times the digit 0 (0:00:00)
-        # wee need 6 times the digits 1 and 2 (11:11:11 to 22:22:22)
+        # wee need 6 times the digits 1 and 2 (11:11:11, 22:22:22)
         # we need 5 times the digits 3, 4, 5 (3:33:33, 4:44:44, 5:55:55)
         # we need 4 times the digits 6, 7, 8, 9 (e.g. 6:16:36, 9:19:59)
         amounts_needed = [5, 6, 6, 5, 5, 5, 4, 4, 4, 4]
@@ -353,66 +359,12 @@ class CalibrationApp(ShowBase):
                 print(f"   digit {i}: {count:2d}, min={amounts_needed[i]}) *** MISSING: {amounts_needed[i] - count} ***")
             else:
                 print(f"   digit {i}: {count:2d}, min={amounts_needed[i]}")
-        warned = [False for _ in range(10)]
-        for i in range(86400):
-            self.check_time(
-                i,
-                warned,
-                callbackMatch = lambda d: None,
-                callbackMiss  = lambda d: None,
-                callbackWarn  = lambda i, time: print(f"   Missing digit {i} for time {time}")
-            )
         print("=======================================================================")
     
-
-    def check_time(self, total_seconds, warned, callbackMatch=None, callbackMiss=None, callbackWarn=None):
-        hours = total_seconds // 3600
-        minutes = (total_seconds % 3600) // 60
-        seconds = total_seconds % 60
-        current_time = f"{hours:02d}{minutes:02d}{seconds:02d}"
-        i = 1 if current_time[0:2] == '00' else 0
-        for item in self.placed_numbers:
-            d = int(current_time[i]) if i < len(current_time) else -1
-            if item['digit'] == d:
-                callbackMatch(item)
-                i += 1
-            else:
-                callbackMiss(item)                
-        if i < len(current_time) and not warned[int(current_time[i])]:
-            digit = int(current_time[i])
-            warned[digit] = True
-            callbackWarn(digit, current_time[0:2] + ':' + current_time[2:4] + ':' + current_time[4:6])
-
-
-    def simulate_time(self, task):
-        # format self.displaySeconds into HH:MM:SS
-        self.check_time(
-            int(task.time * 10) % 86400,
-            self.warned,
-            callbackMatch = lambda d: d['text_node'].setFg((1, 1, 1, 1)),  # white
-            callbackMiss  = lambda d: d['text_node'].setFg((0, 1, 0, 1)),  # green
-            callbackWarn  = lambda i, time: print(f"Warning: Not all digits could be displayed, missing configuration for digit {i} at {time}")
-        )
-        return Task.cont
-
-
-    def toggle_animation(self):
-        """Toggle animation of the clock"""
-        if self.taskMgr.hasTaskNamed("DisplayTimeTask"):
-            task = self.taskMgr.remove("DisplayTimeTask")
-            for item in self.placed_numbers:
-                item['text_node'].setFg((0, 1, 0, 1))  # green
-        else:
-            print("=======================================================================")
-            print("Starting time simulation animation.")
-            self.warned = [False for _ in range(10)]
-            self.taskMgr.add(self.simulate_time, "DisplayTimeTask")
-
 
     def save(self):
         """Save configuration to JSON and quit"""
         config = []
-        
         for item in self.placed_numbers:
             config.append({
                 'digit': item['digit'],
@@ -420,43 +372,60 @@ class CalibrationApp(ShowBase):
                 'y': item['y'],
                 'scale': item['scale']
             })
-        
         config.sort(key=SORT)
-
         # Save to JSON file
         with open(self.config_file, 'w') as f:
             json.dump(config, f, indent=2)
-        
         print(f"Configuration saved to {self.config_file}")
+
+
+    def move(self, dx, dy):
+        """Move spot position by delta"""
+        print(f"Move by ({dx}, {dy})")
+        if self.current_text:
+            x, y = self.current_text.getPos()
+            self.current_text.setPos(x + dx, y + dy)
 
 
     def mouse_move(self, task):
         """Update task for mouse movement"""
         mouse_pos = self.get_mouse_pos_2d()
-        
         if mouse_pos is None:
             return Task.cont
-        
-        if self.dragging and self.last_mouse_pos:
-            # Calculate mouse movement
-            delta = mouse_pos - self.last_mouse_pos            
-            # Scale based on vertical movement
-            if abs(delta.y) > 0.001:
-                self.spot_size = max(0.05, self.spot_size + delta.y * 0.5)
-                self.spot.setScale(self.spot_size)                
-            if self.current_text:
-                self.current_text.setScale(self.spot_size * 2)
-        elif not self.dragging:
-            # Just follow mouse when not dragging
-            self.spot_pos = mouse_pos
-            self.spot.setPos(self.spot_pos.x, 0, self.spot_pos.y)            
-            # If current text exists, move it too
-            if self.current_text:
-                x, y = self.getNumberPosition()
-                self.current_text.setPos(x, y)
-        
-        self.last_mouse_pos = mouse_pos
+        if (mouse_pos.x == self.spot.getX() and mouse_pos.y == self.spot.getZ()):
+            # no mouse movement
+            return Task.cont
+        self.spot.setPos(mouse_pos.x, 0, mouse_pos.y)            
+        # If current text exists, move it too
+        if self.current_text:
+            x, y = self.getNumberPosition()
+            self.current_text.setPos(x, y)        
         return Task.cont
+    
+
+    def resize(self, delta):
+        """Resize current number"""
+        self.spot_size = max(0.05, self.spot_size + delta)
+        self.spot.setScale(self.spot_size)
+        if self.current_text:
+            self.current_text.setScale(self.spot_size * 2)
+    
+
+    def roll_number(self, delta):
+        """Rotate current number"""
+        if self.current_text:
+            self.current_text.setTextR(self.current_text.getTextR() + delta)
+
+
+    def rotate_number(self, delta):
+        """Tilt current number"""
+        if self.current_text:
+            scale = self.current_text.getTextScale()
+            if delta < 0 and (scale[0] < 0.01):
+                return
+            if delta > 0 and (scale[0] > scale[1]):
+                delta = -delta
+            self.current_text.setTextScale((scale[0] + delta, scale[1]))
 
 
 if __name__ == "__main__":
